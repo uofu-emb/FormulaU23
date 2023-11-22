a7e5f7684fd927439eed6c866d1bdff4244a6be3
Ryan Ma <lunaticasian1@gmail.com>
2023-11-22 12:46:48 -0700

    created environments to upload both send and receive code to different boards within single VS window

--- src/receive.c
+++ src/receive.c
@@ -5,7 +5,7 @@
 #include <drivers/can.h>
 #include <drivers/gpio.h>
 #include <sys/byteorder.h>
-
+//
 #define LED0_NODE DT_ALIAS(led0)
 #define LED0    DT_GPIO_LABEL(LED0_NODE, gpios)
 #define PIN0    DT_GPIO_PIN(LED0_NODE, gpios)
@@ -25,16 +25,15 @@ const struct device *led_dev;
 const struct device *led_dev1;
 const struct device *led_dev2;
 
-bool led_status = false;
-
+//message queue to store the incoming CAN Messages
 CAN_DEFINE_MSGQ(bms_queue, 150);
 CAN_DEFINE_MSGQ(imu_queue, 150);
 CAN_DEFINE_MSGQ(mtr_queue, 150);
 
 struct zcan_frame rx_frame;
-int filter_id_imu = 0;
-int filter_id_bms = 0;
-int filter_id_mtr = 0;
+int filter_id_imu;
+int filter_id_bms;
+int filter_id_mtr;
 const struct device *can_dev;
 
 const struct zcan_filter imu_filter = {
@@ -61,32 +60,22 @@ const struct zcan_filter mtr_filter = {
         .id_mask = CAN_EXT_ID_MASK
 };
 
-void recieve_callback(){ // for testing purposes
-    
-    // filter_id_imu = can_attach_msgq(can_dev, &imu_queue, &imu_filter);
-    // filter_id_bms = can_attach_msgq(can_dev, &bms_queue, &bms_filter);
-    filter_id_mtr = can_attach_msgq(can_dev, &mtr_queue, &mtr_filter);
-    // if(filter_id_imu == 0) gpio_pin_set(led_dev, PIN0, (int)true);
-    // if(filter_id_bms == 0) gpio_pin_set(led_dev, PIN1, (int)true);
-    if(filter_id_mtr == 0) gpio_pin_set(led_dev, PIN2, (int)true);
-
-
+void rx_callback_function(struct zcan_frame *frame, void *arg)
+{
+        can_attach_msgq(can_dev, &bms_queue, &bms_filter);
 }
-
 void led_setup()
 {
     led_dev = device_get_binding(LED0);
     led_dev1 = device_get_binding(LED1);
     led_dev2 = device_get_binding(LED2);
-
-    led_status = false;
     gpio_pin_configure(led_dev, PIN0, GPIO_OUTPUT_INACTIVE | FLAGS0);
     gpio_pin_configure(led_dev, PIN1, GPIO_OUTPUT_INACTIVE | FLAGS1);
     gpio_pin_configure(led_dev, PIN2, GPIO_OUTPUT_INACTIVE | FLAGS2);
 
 }
 
-void main(void) {
+void receive_main(void) {
     can_dev = device_get_binding("CAN_1");
 
     led_setup();
@@ -95,29 +84,48 @@ void main(void) {
     // filter_id_bms = can_attach_msgq(can_dev, &bms_queue, &bms_filter);
     // filter_id_mtr = can_attach_msgq(can_dev, &mtr_queue, &mtr_filter);
 
-    if (filter_id_imu < 0 || filter_id_bms < 0 || filter_id_mtr < 0) {
-        printk("Unable to attach isr imu:[%d] bms:[%d] mtr:[%d] ", filter_id_imu,filter_id_bms,filter_id_mtr);
-        return;
-    }
+
+    filter_id_imu = can_attach_isr(can_dev, rx_callback_function, NULL, &imu_filter);
+
+    // if (filter_id_imu < 0 || filter_id_bms < 0 || filter_id_mtr < 0) {
+    //     printk("Unable to attach isr imu:[%d] bms:[%d] mtr:[%d] ", filter_id_imu,filter_id_bms,filter_id_mtr);
+    //     return;
+    // }
 
 
-    while (true) {
+    while (1) {
 
-        recieve_callback();
+        //recieve_callback();
 
-        // if(k_msgq_num_used_get(&imu_queue) > 0){
+        if (filter_id_imu < 0 || filter_id_bms < 0 || filter_id_mtr < 0) 
+        {
+            printk("Unable to attach isr imu:[%d] bms:[%d] mtr:[%d] ", filter_id_imu,filter_id_bms,filter_id_mtr);
+
+            return;
+        }
+
+        if(filter_id_bms > 0){
         
-        //     k_msgq_get(&bms_queue, &rx_frame, K_MSEC(50));
-        //     // gpio_pin_set(led_dev, PIN0, (int)true);
-        // }
-        // if(k_msgq_num_used_get(&bms_queue) > 0){
-        //     k_msgq_get(&imu_queue, &rx_frame, K_MSEC(60));
-        //     // gpio_pin_set(led_dev1, PIN1, (int)true);
-        // }
-        // if(k_msgq_num_used_get(&mtr_queue) > 0){
-        //     k_msgq_get(&mtr_queue, &rx_frame, K_MSEC(70));
-        //     // gpio_pin_set(led_dev2, PIN2, (int)true);
-        // }
+            //k_msgq_get(&bms_queue, &rx_frame, K_MSEC(50));
+            gpio_pin_set(led_dev, PIN0, (int)true);
+            k_msleep(100);
+            gpio_pin_set(led_dev, PIN0, (int)false);
+            k_msleep(100);
+        }
+        if(filter_id_imu > 0){
+            //k_msgq_get(&imu_queue, &rx_frame, K_MSEC(60));
+            gpio_pin_set(led_dev1, PIN1, (int)true);
+            k_msleep(100);
+            gpio_pin_set(led_dev1, PIN1, (int)false);
+            k_msleep(100);
+        }
+        if(filter_id_mtr > 0){
+            //k_msgq_get(&mtr_queue, &rx_frame, K_MSEC(70));
+            gpio_pin_set(led_dev2, PIN2, (int)true);
+            k_msleep(100);
+            gpio_pin_set(led_dev2, PIN2, (int)false);
+            k_msleep(100);
+        }
 
         //TODO Here SPI conversion here
     }
