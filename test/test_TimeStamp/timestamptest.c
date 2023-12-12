607063677bc3925006709e8a7d86ada6d02deb1b
Misael Nava <misaelnava812@gmail.com>
2023-12-11 19:17:49 -0700

    Remove parts of the tests

--- test/test_TimeStamp/timestamptest.c
+++ test/test_TimeStamp/timestamptest.c
@@ -11,20 +11,20 @@ void setUp(void) {
 void tearDown(void) {}
 
 void test_reference_with_delay(){
-    int32_t expected = 0x00000005;
+    int32_t expected = 0x00000004;
     setup_RTC();
     //printk("\nStep 1\n");
     //resetTime();
     //printk("\nStep 2\n");
-    while(1){
+    //while(1){
     k_sleep(K_SECONDS(5));
     //printk("\nStep 3\n");
     int32_t timeStamp = getTimeStamp();
     //printk("\nStep 4\n");
     printk("\nTime stamp at: %x\n", timeStamp);
-    }
+    //}
     //TEST_ASSERT_EQUAL_INT32(expected, timeStamp);
-    TEST_ASSERT_EQUAL_INT32(expected, 0x00000005);
+    TEST_ASSERT_EQUAL_INT32(expected, timeStamp);
 }
 
 int main (void)
