a783090cf7cb287edada5200bd465334e37c5695
Ryan Ma <89541272+LunaticAsian@users.noreply.github.com>
2023-12-18 11:50:41 -0700

    Update timestamp.c with sleeps to allow other thread operations

--- lib/TimeStamp/src/timestamp.c
+++ lib/TimeStamp/src/timestamp.c
@@ -18,19 +18,19 @@ void setup_RTC(void){
 
     LL_RTC_DisableWriteProtection(RTC);
     LL_RTC_EnableInitMode(RTC);
-    while(!LL_RTC_IsActiveFlag_INIT(RTC)){};
+    while(!LL_RTC_IsActiveFlag_INIT(RTC)){k_msleep(500);};
     LL_RTC_SetAsynchPrescaler(RTC, async_pre);
     LL_RTC_SetSynchPrescaler(RTC, sync_pre);
     LL_RTC_TIME_SetFormat(RTC, LL_RTC_TIME_FORMAT_AM_OR_24);
     LL_RTC_DisableInitMode(RTC);
-    while(LL_RTC_IsActiveFlag_RECALP(RTC)){};
+    while(LL_RTC_IsActiveFlag_RECALP(RTC)){k_msleep(500);};
     LL_RTC_CAL_SetMinus(RTC, (uint32_t)482);
     LL_RTC_CAL_SetPulse(RTC, LL_RTC_CALIB_INSERTPULSE_SET);
     LL_RTC_EnableWriteProtection(RTC);
 }
 
 uint32_t getTimeStamp(void){
-    while(!LL_RTC_IsActiveFlag_RS(RTC)){};
+    while(!LL_RTC_IsActiveFlag_RS(RTC)){k_msleep(500);};
     return LL_RTC_TIME_Get(RTC);
     //return LL_RTC_TS_GetTime(RTC);
 }
